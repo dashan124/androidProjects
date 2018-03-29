3a2d3f1aaa683b2f5320ac6520d988f89dcbd1ed
dashan124 <dashanjotsingh961999@gmail.com>
2018-03-29 11:07:11 +0530

    reading data from shared preferences

--- MusicVisual/app/src/main/java/android/example/com/visualizerpreferences/VisualizerActivity.java
+++ MusicVisual/app/src/main/java/android/example/com/visualizerpreferences/VisualizerActivity.java
@@ -4,11 +4,14 @@ package android.example.com.visualizerpreferences;
 
 import android.Manifest;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.example.com.visualizerpreferences.AudioVisuals.AudioInputReader;
 import android.example.com.visualizerpreferences.AudioVisuals.VisualizerView;
 import android.os.Build;
 import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -32,7 +35,12 @@ public class VisualizerActivity extends AppCompatActivity {
     }
 
     private void defaultSetup() {
-        mVisualizerView.setShowBass(true);
+
+        SharedPreferences sharedPreferences= PreferenceManager.getDefaultSharedPreferences(this);
+
+        mVisualizerView.setShowBass(sharedPreferences.getBoolean("show_bass",true));
+
+
         mVisualizerView.setShowMid(true);
         mVisualizerView.setShowTreble(true);
         mVisualizerView.setMinSizeScale(1);
