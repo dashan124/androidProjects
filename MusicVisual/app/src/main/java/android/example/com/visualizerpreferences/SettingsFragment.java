b5cf03d91419a7177ea0ed2eb3462415001926b0
dashan124 <dashanjotsingh961999@gmail.com>
2018-03-29 13:15:51 +0530

    added color options

--- MusicVisual/app/src/main/java/android/example/com/visualizerpreferences/SettingsFragment.java
+++ MusicVisual/app/src/main/java/android/example/com/visualizerpreferences/SettingsFragment.java
@@ -1,17 +1,71 @@
 package android.example.com.visualizerpreferences;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.preference.CheckBoxPreference;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
+import android.support.v7.preference.PreferenceScreen;
 
 /**
  * Created by dashan on 29/3/18.
  */
 
-public class SettingsFragment extends PreferenceFragmentCompat {
+public class SettingsFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener{
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
     addPreferencesFromResource(R.xml.pref_visulizer);
 
+        SharedPreferences sharedPreferences=getPreferenceScreen().getSharedPreferences();
+        PreferenceScreen preferenceScreen=getPreferenceScreen();
+
+        int count=preferenceScreen.getPreferenceCount();
+
+        for(int i=0;i<count;i++){
+            Preference p=preferenceScreen.getPreference(i);
+
+            if(!(p instanceof CheckBoxPreference)){
+                String value=sharedPreferences.getString(p.getKey(),"");
+                setPreferenceSummary(p,value);
+            }
+        }
+    }
+
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        Preference preference = findPreference(key);
+        if (null != preference) {
+            // Updates the summary for the preference
+            if (!(preference instanceof CheckBoxPreference)) {
+                String value = sharedPreferences.getString(preference.getKey(), "");
+                setPreferenceSummary(preference, value);
+            }
+        }
+    }
+    private void setPreferenceSummary(Preference preference,String value){
+        if(preference instanceof ListPreference){
+            ListPreference listPreference=(ListPreference)preference;
+            int prefIndex=listPreference.findIndexOfValue(value);
+            if(prefIndex>=0){
+                listPreference.setSummary(listPreference.getEntries()[prefIndex]);
+            }
+        }
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getPreferenceScreen().getSharedPreferences()
+                .registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        getPreferenceScreen().getSharedPreferences()
+                .unregisterOnSharedPreferenceChangeListener(this);
     }
 }
