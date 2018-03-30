14e83a815115f3606c03760f0ff9cf2853905fdf
dashan124 <dashanjotsingh961999@gmail.com>
2018-03-30 19:45:43 +0530

    ui for login added

--- UIHome/app/src/main/java/com/example/dashan/uihome/Home.java
+++ UIHome/app/src/main/java/com/example/dashan/uihome/Home.java
@@ -3,7 +3,7 @@ package com.example.dashan.uihome;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Home extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
