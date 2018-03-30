0969320ddd62f1aab34178c7afda3473792472d4
dashan124 <dashanjotsingh961999@gmail.com>
2018-03-30 09:14:22 +0530

    new project sql database added

---
+++ SqlWaitlist/app/src/main/java/com/example/android/waitlist/data/WaitlistDbHelper.java
@@ -0,0 +1,13 @@
+package com.example.android.waitlist.data;
+
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by dashan on 30/3/18.
+ */
+
+public class WaitlistDbHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME="waitlist.db";
+    
+}
